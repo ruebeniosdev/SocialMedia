33e8fcbae3e0eddf3990f7768d75a1aae819a877
Rueben <akankobatengr@gmail.com>
2023-04-29 19:40:25 +0000

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Rueben on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
